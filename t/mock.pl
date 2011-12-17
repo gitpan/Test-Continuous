3a140baae124f49f8d748863e7797539d3d90435
劉康民 <gugod@gugod.org>
2011-12-17 09:45:36 +0000

    Import of GUGOD/Test-Continuous-0.72 from CPAN.
    gitpan-cpan-distribution: Test-Continuous
    gitpan-cpan-version:      0.72
    gitpan-cpan-path:         GUGOD/Test-Continuous-0.72.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- t/mock.pl
+++ t/mock.pl
@@ -1,11 +1,16 @@
-our @notified;
-{
-    no strict;
-    no warnings;
+use FindBin;
+use File::Temp;
+use YAML;
 
-    sub Test::Continuous::Notifier::send_notify {
-        my (undef, $msg) = @_;
-        push @notified, $msg;
-    }
+$ENV{PERL5LIB} = join(":", $FindBin::Bin."/lib", $FindBin::Bin."/../lib", $ENV{PERL5LIB});
+
+my $temp = File::Temp->new;
+$temp->unlink_on_destroy(0);
+
+$ENV{PERL_TEST_CONTINUOUS_TEST_NOTIFY_OUTPUT} = $temp->filename;
+
+sub read_notifications {
+    YAML::LoadFile($ENV{PERL_TEST_CONTINUOUS_TEST_NOTIFY_OUTPUT});
 }
+
 1;
